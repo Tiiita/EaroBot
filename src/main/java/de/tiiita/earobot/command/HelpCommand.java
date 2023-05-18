c76cecc4ed07dd785642ad835baf0638bfe2d3a8
Tiiita <96084154+Tiiita@users.noreply.github.com>
2023-05-18 22:09:01 +0200

    Fix Help Command

--- src/main/java/de/tiiita/earobot/command/HelpCommand.java
+++ src/main/java/de/tiiita/earobot/command/HelpCommand.java
@@ -31,13 +31,13 @@ public class HelpCommand extends ListenerAdapter {
         embedBuilder.setTitle("Help");
         embedBuilder.setColor(Color.WHITE);
         embedBuilder.setDescription("Here are some important information!");
-        embedBuilder.addField("Configure Bot", "The bot has _4_ main features: \n\n"
+        embedBuilder.addField("Configure Bot", "The bot has _5_ main features: \n\n"
                 + "- Send Welcome messages (1)\n"
                 + "- Reacting to ideas (2)\n"
                 + "- Announce Updates (3)\n"
-                + "- Ticket system (4)"
+                + "- Ticket system (4)\n"
                 + "- Clear discord from raids (5)\n\n"
-                + "(1) Go into the channel you want the messages and type **/set-welcome-channel**\n"
+                + "(1) Go into the channel you want the messages and type **/set-welcome-channel**.\n"
                 + "Done? Every user join and leave will be send in this channel!\n\n"
                 + "(2) You can choose the reaction channel with **/set-ideas-channel**\n\n"
                 + "(3) You can announce updates in the channel you are in with **/update**\n\n"
@@ -45,7 +45,7 @@ public class HelpCommand extends ListenerAdapter {
                 + "(5) You have to set a ticket role with **/set-ticket-role**\n"
                 + "Done with that? To complete the ticket setup, go into a channel where you\n"
                 + "want the panel and type **/send-ticket-message**. A Panel will show up. Done!\n"
-                + "Currently 2 panels will show. This is a bug... Please delete one!", false);
+                + "_Currently more than 1 panel will show. This is a bug... Please delete the others!_", false);
 
         embedBuilder.addField("Bot Information",
                 "Author: Tiiita\n"
