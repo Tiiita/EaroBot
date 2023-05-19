d9bd920e02c12c22727f3175df41b4c29c25485b
Tiiita <96084154+Tiiita@users.noreply.github.com>
2023-05-19 21:49:29 +0200

    Final fix for update command

--- src/main/java/de/tiiita/earobot/command/UpdateCommand.java
+++ src/main/java/de/tiiita/earobot/command/UpdateCommand.java
@@ -53,7 +53,7 @@ public class UpdateCommand extends ListenerAdapter {
                     .setRequired(true)
                     .setPlaceholder("Write here the information in german :)")
                     .build();
-            TextInput portugueseInput = TextInput.create("portuguese-input", "Portuguese", TextInputStyle.PARAGRAPH)
+            TextInput portugueseInput = TextInput.create("portuguese-input", "Português", TextInputStyle.PARAGRAPH)
                     .setMinLength(1)
                     .setRequired(false)
                     .setMaxLength(4000)
@@ -82,7 +82,7 @@ public class UpdateCommand extends ListenerAdapter {
         embed.addField("» \uD83C\uDDEC\uD83C\uDDE7 English", englishInput, false);
         embed.addField("» \uD83C\uDDE9\uD83C\uDDEA Deutsch", germanInput, false);
         if (!portugueseInput.equalsIgnoreCase("")) {
-            embed.addField("» \uD83C\uDDE7\uD83C\uDDF7 Portuguese", germanInput, false);
+            embed.addField("» \uD83C\uDDE7\uD83C\uDDF7 Português", germanInput, false);
         }
         embed.setThumbnail(event.getJDA().getSelfUser().getAvatarUrl());
         embed.setFooter("earomc.net", event.getJDA().getSelfUser().getAvatarUrl());
