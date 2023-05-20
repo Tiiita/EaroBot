a14309bda03989c3d9fa85d3af1ed8e3582f3040
Tiiita <96084154+Tiiita@users.noreply.github.com>
2023-05-20 13:42:03 +0200

    Try new design on update command

--- src/main/java/de/tiiita/earobot/command/commands/UpdateCommand.java
+++ src/main/java/de/tiiita/earobot/command/commands/UpdateCommand.java
@@ -53,7 +53,7 @@ public class UpdateCommand extends ListenerAdapter {
                     .setRequired(true)
                     .setPlaceholder("Write here the information in german :)")
                     .build();
-            TextInput portugueseInput = TextInput.create("portuguese-input", "Português", TextInputStyle.PARAGRAPH)
+            TextInput portugueseInput = TextInput.create("portuguese-input", "Português", TextInputStyle.UNKNOWN)
                     .setMinLength(1)
                     .setRequired(false)
                     .setMaxLength(4000)
